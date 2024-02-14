5fdd18630084754035c7f396e66695e2caf6c24a
madhurpv <madhurpreetamvaidya@gmail.com>
2024-02-14 11:43:34 +0530

    Added Activity Home

--- app/src/main/java/com/mv/cp_dbms/HomeActivity.java
+++ app/src/main/java/com/mv/cp_dbms/HomeActivity.java
@@ -1,14 +1,27 @@
 package com.mv.cp_dbms;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
 
 public class HomeActivity extends AppCompatActivity {
 
+
+    CardView votingCardView, guestsCardView, noticesCardView, feesCardView, complaintsCardView, profileCardView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+
+        votingCardView = findViewById(R.id.votingCardView);
+        guestsCardView = findViewById(R.id.guestsCardView);
+        noticesCardView = findViewById(R.id.noticesCardView);
+        feesCardView = findViewById(R.id.feesCardView);
+        complaintsCardView = findViewById(R.id.complaintsCardView);
+        profileCardView = findViewById(R.id.profileCardView);
     }
 }
